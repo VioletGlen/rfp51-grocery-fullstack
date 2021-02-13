706e636dc749564bfbe348d7709a3a65e76bda41
Laura Nielsen <lnielsen.wi@gmail.com>
2021-02-13 13:27:40 -0600

    server index, schema.sql, db index

--- schema.sql
+++ schema.sql
@@ -1,3 +1,21 @@
-/*  Execute this file from the command line by typing:
- *    sqlite3 items.db < schema.sql
- *  to create the database and the tables.*/
+DROP DATABASE IF EXISTS grocery;
+
+CREATE DATABASE grocery;
+
+USE grocery;
+
+CREATE TABLE groceryList (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  itemName VARCHAR(50) NOT NULL,
+  quantity INT NOT NULL
+);
+
+-- /*  Execute this file from the command line by typing:
+--  *    sqlite3 items.db < schema.sql
+--  *  to create the database and the tables.*/
+
+-- Parker Stafford: "Just to clarify for anyone who did not join the junior zoom room earlier. Please use mysql for your database. If you get errors when running npm install please remove sqlite3 from you dependencies in package.json!"
+
+-- /*  Execute this file from the command line by typing:
+--  *    mysql -u student -p < schema.sql
+--  *  to create the database and the tables.*/
--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 // YOUR SERVER CODE HERE
 
 const express = require("express");
-const app = express();
+const app = express(); //returns an object w methods i.e. .get .listen
 const port = 8080;
 const cors = require("cors");
 const db = require("../db/index.js");
 
 app.use(cors());
 
-app.listen(port, () => {
-  console.log("listening on 8080");
+app.get('/', (req, res) => {
+  res.send(`Hello World!`); //sends to http://localhost:8080/
+  //res.status(200);
+  //res.write(`Hello World!`);
+  //res.end
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err, `Error connecting to server`);
+  } else {
+    console.log(`Listening on localhost:${port}`);
+  }
 });
